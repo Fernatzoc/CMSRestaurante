df474ccf3e335f40ace692bbab92700cbfdf5b3a
Fernando Tzoc <fernandotzoc4@gmail.com>
2021-09-23 21:48:34 -0600

    Sass Fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js').vue({ version: 3, extractVueStyles: true })
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css', {
+        sassOptions: {
+            quietDeps: true,
+        },
+    })
     .sourceMaps();
