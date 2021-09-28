68c75eaa1cfc4c98edb0acbb1c4dedeb0c5e938c
Fernando Tzoc <fernandotzoc4@gmail.com>
2021-09-27 21:53:12 -0600

    Fix

--- routes/admin.php
+++ routes/admin.php
@@ -10,9 +10,4 @@ Route::get('', [HomeController::class, 'index']);
 
 Route::get('/empresa', [EmpresaController::class, 'index' ])-> name('empresa.index');
 Route::put('/empresa', [EmpresaController::class, 'update'])->name('empresa.update');
-/* Route::get('/recetas/create', [RecetaController::class, 'create' ])->name('recetas.create');
-Route::post('/recetas', [RecetaController::class, 'store'])->name('recetas.store');
-Route::get('/recetas/{receta}', [RecetaController::class, 'show'])->name('recetas.show');
-Route::get('/recetas/{receta}/edit', [RecetaController::class, 'edit'])->name('recetas.edit');
-Route::put('/recetas/{receta}', [RecetaController::class, 'update'])->name('recetas.update');
-Route::delete('/recetas/{receta}', [RecetaController::class, 'destroy'])->name('recetas.destroy'); */
+
