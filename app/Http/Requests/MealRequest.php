16f7455c2c78f1b69fa1faab5b285dd7c2129e45
Rodrigo <rodrigo.arturojuarez@gmail.com>
2021-12-05 12:57:11 -0600

    se finaliza menu categorias en la pagina de menu

--- app/Http/Requests/MealRequest.php
+++ app/Http/Requests/MealRequest.php
@@ -25,7 +25,6 @@ class MealRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'description' => 'required',
             'price' => 'required'
 
         ];
